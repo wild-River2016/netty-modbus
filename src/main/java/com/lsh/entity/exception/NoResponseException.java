e54937e2f86ebba74449956c520af452d5762398
lsh1990 <xtlsh1990@126.com>
2019-04-11 22:12:19 +0800

    add 返回值异常信息类

---
+++ src/main/java/com/lsh/entity/exception/NoResponseException.java
@@ -0,0 +1,12 @@
+package com.lsh.entity.exception;
+
+/**
+ * @ClassName NoResponseException
+ * @Description: 无返回信息异常
+ * @Author lsh
+ * @Date 2019/4/11 22:03
+ * @Version
+ */
+public class NoResponseException extends Exception{
+
+}
