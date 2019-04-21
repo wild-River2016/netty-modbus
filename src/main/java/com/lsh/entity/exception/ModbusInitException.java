697631e79661e85cdf97f64c005cd463b47707db
lsh <lsh>
2019-04-21 22:17:18 +0800

    add 异常处理类

---
+++ src/main/java/com/lsh/entity/exception/ModbusInitException.java
@@ -0,0 +1,68 @@
+package com.lsh.entity.exception;
+
+import java.security.PrivilegedActionException;
+
+/**
+ * @ClassName ModbusInitException
+ * @Description: 初始化异常
+ * @Author lsh
+ * @Date 2019/4/20 19:34
+ * @Version
+ */
+public class ModbusInitException extends Exception {
+    /**
+     * Constructs a new exception with {@code null} as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
+    public ModbusInitException() {
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public ModbusInitException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message and
+     * cause.  <p>Note that the detail message associated with
+     * {@code cause} is <i>not</i> automatically incorporated in
+     * this exception's detail message.
+     *
+     * @param message the detail message (which is saved for later retrieval
+     *                by the {@link #getMessage()} method).
+     * @param cause   the cause (which is saved for later retrieval by the
+     *                {@link #getCause()} method).  (A <tt>null</tt> value is
+     *                permitted, and indicates that the cause is nonexistent or
+     *                unknown.)
+     * @since 1.4
+     */
+    public ModbusInitException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new exception with the specified cause and a detail
+     * message of <tt>(cause==null ? null : cause.toString())</tt> (which
+     * typically contains the class and detail message of <tt>cause</tt>).
+     * This constructor is useful for exceptions that are little more than
+     * wrappers for other throwables (for example, {@link
+     * PrivilegedActionException}).
+     *
+     * @param cause the cause (which is saved for later retrieval by the
+     *              {@link #getCause()} method).  (A <tt>null</tt> value is
+     *              permitted, and indicates that the cause is nonexistent or
+     *              unknown.)
+     * @since 1.4
+     */
+    public ModbusInitException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/com/lsh/entity/exception/ModbusTransportException.java
@@ -0,0 +1,19 @@
+package com.lsh.entity.exception;
+
+import java.security.PrivilegedActionException;
+
+/**
+ * @ClassName ModbusTransportException
+ * @Description: TODO
+ * @Author lsh
+ * @Date 2019/4/20 22:09
+ * @Version
+ */
+public class ModbusTransportException extends Exception {
+
+    public ModbusTransportException(String message) {
+        super(message);
+
+    }
+
+}
