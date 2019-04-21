5c440d464066809f3025851b0c43a72bb60bce90
lsh <lsh>
2019-04-21 22:15:57 +0800

    add 增加modbus功能码

---
+++ src/main/java/com/lsh/code/FunctionCode.java
@@ -0,0 +1,27 @@
+package com.lsh.code;
+
+/**
+ * @ClassName FunctionCode
+ * @Description: 功能码
+ * @Author lsh
+ * @Date 2019/4/21 17:29
+ * @Version
+ */
+public class FunctionCode {
+
+    public static final short READ_COILS = 0x01;
+    public static final short READ_DISCRETE_INPUTS = 0x02;
+    public static final short READ_HOLDING_REGISTERS = 0x03;
+    public static final short READ_INPUT_REGISTERS = 0x04;
+    public static final short WRITE_SINGLE_COIL = 0x05;
+    public static final short WRITE_SINGLE_REGISTER = 0x06;
+    public static final short WRITE_MULTIPLE_COILS = 0x0F;
+    public static final short WRITE_MULTIPLE_REGISTERS = 0x10;
+    public static final short READ_FILE_RECORD = 0x14;
+    public static final short WRITE_FILE_RECORD = 0x15;
+    public static final short MASK_WRITE_REGISTER = 0x16;
+    public static final short READ_WRITE_MULTIPLE_REGISTERS = 0x17;
+    public static final short READ_FIFO_QUEUE = 0x18;
+    public static final short ENCAPSULATED_INTERFACE_TRANSPORT = 0x2B;
+
+}
